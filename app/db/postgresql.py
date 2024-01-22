93a917e8b4e7e595b8ff1a0ed1d82c4bb068f77d
Bish0p-r <bish0pd2138@gmail.com>
2024-01-23 03:54:39 +0800

    Fixed the db configuration.

--- app/db/postgresql.py
+++ app/db/postgresql.py
@@ -1,10 +1,10 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
 async_engine = create_async_engine(settings.db_uri, echo=False)
-async_session_maker = sessionmaker(bind=async_engine, class_=AsyncSession)
+async_session_maker = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
