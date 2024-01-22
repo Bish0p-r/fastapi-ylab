97927f693c12852106057862359bd7e9fb689cdc
Bish0p-r <bish0pd2138@gmail.com>
2024-01-23 03:55:22 +0800

    Deleted an unused import.

--- app/db/postgresql.py
+++ app/db/postgresql.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 
 from app.config import settings
 
