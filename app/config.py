290f95196e70026f5095f9d0d627d859a2a9ef8c
Bish0p-r <bish0pd2138@gmail.com>
2024-01-25 06:36:48 +0800

    Set up an environment for testing.

--- app/config.py
+++ app/config.py
@@ -18,5 +18,19 @@ class Settings(BaseSettings):
     def db_uri(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
+    @property
+    def test_db_uri(self):
+        return (
+            f"postgresql+asyncpg://{self.TEST_DB_USER}:"
+            f"{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:"
+            f"{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+        )
+
 
 settings = Settings()
--- app/db/postgresql.py
+++ app/db/postgresql.py
@@ -1,10 +1,20 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
                                     create_async_engine)
 from sqlalchemy.orm import DeclarativeBase
 
 from app.config import settings
 
-async_engine = create_async_engine(settings.db_uri, echo=False)
+
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.test_db_uri
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.db_uri
+    DATABASE_PARAMS = {}
+
+
+async_engine = create_async_engine(DATABASE_URL, echo=False, **DATABASE_PARAMS)
 async_session_maker = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
 
