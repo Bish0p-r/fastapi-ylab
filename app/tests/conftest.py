ad232f4341a2c38881db3dcfdd721160a95be775
Bish0p-r <bish0pd2138@gmail.com>
2024-01-25 22:31:19 +0800

    Added event_loop fixture.

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 from httpx import AsyncClient
 
@@ -19,6 +21,14 @@ async def prepare_database():
 async def ac():
     async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
         yield ac
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture
