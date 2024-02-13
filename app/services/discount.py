4d1a3f04681dc19e4c9a8317fb2ac5492babc15e
Bish0p-r <bish0pd2138@gmail.com>
2024-02-14 00:55:46 +0800

    Created a separate service for discounts.

---
+++ app/services/discount.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+from app.models.dish import Dish
+from app.services.cache import CacheService
+
+
+class DiscountServices:
+    def __init__(self, cache_service: CacheService) -> None:
+        self.cache_service = cache_service
+
+    async def set_discount(self, *dishes: Dish) -> None:
+        for dish in dishes:
+            discount = await self.cache_service.get_cache(f'discount:{dish.id}')
+            if discount is not None:
+                discount = int(discount)
+                dish.price = Decimal(dish.price * (100 - discount) / 100).quantize(Decimal('1.00'))
