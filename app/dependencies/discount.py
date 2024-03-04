bef9850986b43f106303fe7023852d31e11aa4f1
Bish0p-r <bish0pd2138@gmail.com>
2024-03-04 19:12:51 +0800

    Fixed typehints in dependencies.

--- app/dependencies/discount.py
+++ app/dependencies/discount.py
@@ -2,13 +2,12 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from app.common.abstract.services.discount import AbstractDiscountServices
 from app.services.cache import cache_service
 from app.services.discount import DiscountServices
 
 
-async def get_discount_services() -> AbstractDiscountServices:
+async def get_discount_services() -> DiscountServices:
     return DiscountServices(cache_service=cache_service)
 
 
-GetDiscountServices = Annotated[AbstractDiscountServices, Depends(get_discount_services)]
+GetDiscountServices = Annotated[DiscountServices, Depends(get_discount_services)]
--- app/dependencies/menu.py
+++ app/dependencies/menu.py
@@ -2,17 +2,16 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from app.common.abstract.services.menu import AbstractMenuServices
 from app.dependencies.discount import get_discount_services
 from app.repositories.menu import MenuRepository
 from app.services.cache import cache_service
 from app.services.menu import MenuServices
 
 
-async def get_menu_services() -> AbstractMenuServices:
+async def get_menu_services() -> MenuServices:
     return MenuServices(
         repository=MenuRepository, cache_service=cache_service, discount_service=await get_discount_services()
     )
 
 
-GetMenuServices = Annotated[AbstractMenuServices, Depends(get_menu_services)]
+GetMenuServices = Annotated[MenuServices, Depends(get_menu_services)]
